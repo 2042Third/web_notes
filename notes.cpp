9033939fcacb711c5568cb4ba281121afeeee42c
mike yang <18604713262@163.com>
2021-10-30 16:12:41 -0400

    script commit

--- notes.cpp
+++ notes.cpp
@@ -6,20 +6,22 @@
 
 using namespace std;
 
-void set_up(vector<uint8_t>& buf, string inp){
+void set_up(vector<uint8_t>* buf, string inp){
   for(char a:inp){
-    buf.push_back((uint8_t)a);
+    buf->push_back((uint8_t)a);
   }
 }
 int main(){
   cout<<"First note!\n"<<endl;
   string key = "1234";
   string input="this is a secret\npassword:1234\n";
-  vector<uint8_t> buf;
-  vector<uint8_t> outstr;
+  vector<uint8_t>* buf = new vector<uint8_t>();
+  vector<uint8_t>* outstr = new vector<uint8_t>();
   set_up(buf,input);
   // DE=0;
-  cmd_enc(buf.data(),input.size()+2,outstr.data(),key);
+  cmd_enc(buf->data(),input.size()+2,outstr->data(),key);
   cout<<"compile complete: ";
-  cout<<(char*)outstr.data()<<endl;
+  cout<<(char*)outstr->data()<<endl;
+  delete(buf);
+  delete(outstr);
 }
