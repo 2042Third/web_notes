6bd542877270a8e544400618c485e5c9259dd1c2
mike yang <18604713262@163.com>
2021-11-05 14:50:22 -0400

    script commit

--- WEB-INF/classes/CORSFilter.java
+++ WEB-INF/classes/CORSFilter.java
@@ -1,4 +1,4 @@
-package cors;
+package com.common;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,38 +10,15 @@ public class CORSFilter implements Filter {
     public void destroy() {
     }
     public static String VALID_METHODS = "DELETE, HEAD, GET, OPTIONS, POST, PUT";
-
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        HttpServletRequest httpReq = (HttpServletRequest) req;
-        HttpServletResponse httpResp = (HttpServletResponse) resp;
-
-        // No Origin header present means this is not a cross-domain request
-        String origin = httpReq.getHeader("Same-Origin");
-         if (origin == null) {
-            // Return standard response if OPTIONS request w/o Origin header
-           if ("OPTIONS".equalsIgnoreCase(httpReq.getMethod())) {
-                httpResp.setHeader("Allow", VALID_METHODS);
-                httpResp.setStatus(200);
-                return;
-            }
-        } else {
-            // This is a cross-domain request, add headers allowing access
-            httpResp.setHeader("Access-Control-Allow-Origin", origin);
-            httpResp.setHeader("Access-Control-Allow-Methods", VALID_METHODS);
-
-            String headers = httpReq.getHeader("Access-Control-Request-Headers");
-            if (headers != null)
-                httpResp.setHeader("Access-Control-Allow-Headers", headers);
-
-            // Allow caching cross-domain permission
-            httpResp.setHeader("Access-Control-Max-Age", "3600");
-        }
-        // Pass request down the chain, except for OPTIONS
-        if (!"OPTIONS".equalsIgnoreCase(httpReq.getMethod())) {
-            chain.doFilter(req, resp);
-        }
- }
-
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+    
     public void init(FilterConfig config) throws ServletException {
 
     }
