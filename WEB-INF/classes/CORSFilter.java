604163e3c18e1b665c48e9e201ced6f956d1c846
mike yang <18604713262@163.com>
2021-11-05 16:37:07 -0400

    script commit

--- WEB-INF/classes/CORSFilter.java
+++ WEB-INF/classes/CORSFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HttpResponseHeaderExample extends HttpServlet {
+public class CORSFilter extends HttpServlet {
 
 protected void doGet(HttpServletRequest request,
 HttpServletResponse response) throws ServletException, IOException {
