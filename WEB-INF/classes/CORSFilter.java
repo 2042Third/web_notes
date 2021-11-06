f69f3bf516729ef6d1cf8724f9a0e19e9618ce6d
mike yang <18604713262@163.com>
2021-11-06 11:50:19 -0400

    script commit

--- WEB-INF/classes/CORSFilter.java
+++ WEB-INF/classes/CORSFilter.java
@@ -4,7 +4,8 @@ import javax.servlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
-
+import java.util.Scanner;
+import java.io.File;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
