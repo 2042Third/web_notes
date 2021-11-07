fd2f638c4221769146dc396d260c259c37cf1b97
mike yang <18604713262@163.com>
2021-11-07 11:42:48 -0500

    script commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+        $( document ).ready(function() {
+            Module.onRuntimeInitialized= function() {
+                
+                var vec = new Module.CharList();
+
+
+
+                let inputVal = "this is not a secret, but.";
+                let text_key = "1234";
+                for (let character of inputVal) {
+                  vec.push_back(character);
+                }
+                var enc = new Module.Cc20();
+
+                Console.log( Module.use_vector_string(vec));
+
+
+                var input_t = Array.from(inputVal);
+                var outstr = new Array((inputVal.length+13)*"");
+                var outstr_dec = new Array((inputVal.length)*"");
+                enc.cmd_enc(input_t,inputVal.length,outstr,text_key);
+                enc.cmd_enc(outstr,inputVal.length+12,outstr_dec,text_key);
+                $("#output1").append(Module.loader_check());
+                enc.delete();
+                vec.delete();
+            }
+
+            var data = new FormData();
+            data.append('listings','files list');
+            
+            $('#button1').click(function() {
+
+               let inputVal = document.getElementsByClassName("inputClass2")[0].value;
+              let text_key = document.getElementsByClassName("inputClass1")[0].value;
+              var input_t = Array.from(inputVal);
+              var outstr = new Array((inputVal.length+13)*"");
+              var outstr_dec = new Array((inputVal.length)*"");
+              Module.cmd_enc(input_t,inputVal.length,outstr,text_key);
+              Module.cmd_enc(outstr,inputVal.length+12,outstr_dec,text_key);
+              $("#output1").append(outstr_dec.toString());
+            });
+
+
+        });
