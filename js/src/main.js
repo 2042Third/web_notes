957b1e11bfaf33a5748a661dccaa9c2e189b090f
mike yang <18604713262@163.com>
2021-11-14 15:13:35 -0500

    script commit

--- js/src/main.js
+++ js/src/main.js
@@ -2,6 +2,7 @@
 var Module = {
     onRuntimeInitialized: function () {
         var a = Module.loader_check("1234", "12345");
+        console.log(a);
         var b = Module.loader_out("1234", a);
     }
 };
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 var Module = {
       onRuntimeInitialized: function() {
         var a= Module.loader_check("1234","12345");
+        console.log(a);
         var b = Module.loader_out("1234",a);
       }
     };
