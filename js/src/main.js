a5bf42fd9dd625e419f3ac025f43f3ecf976656b
mike yang <18604713262@163.com>
2021-11-22 15:38:28 -0500

    script commit

--- js/src/main.js
+++ js/src/main.js
@@ -1,10 +1,12 @@
 "use strict";
+$(document).ready(function () {
+    $("#output").append("<font color=\"green\">请输入密码和用户名<br></font>");
+});
 var Module = {
     onRuntimeInitialized: function () {
         var a = Module.loader_check("1234", "hello, how are you?");
         console.log("in javascript: " + a);
         var b = Module.loader_out("1234", a);
         console.log("in javascript: " + b);
-        $("#output").append("请输入密码和用户名");
     }
 };
