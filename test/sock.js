4653265f231cf344d980f20197755c78d14988ce
mike yang <18604713262@163.com>
2021-12-13 14:32:22 -0500

    script commit

---
+++ test/sock.js
@@ -0,0 +1,33 @@
+var username = `user_${getRandomNumber()}`
+const socket = io();
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 1000);
+}
+
+$(() => {
+    getMessages();
+    $("#send").click(() => {
+        const message = {
+            name: $("#name").val(),
+            message: $("#message").val()
+        };
+        postMessage(message);
+    });
+});
+
+socket.on('message', addMessage);
+
+function addMessage({ name, message }) {
+    $("#messages").append(`<h4>${name}</h4><p>${message}</p>`);
+}
+
+function getMessages() {
+    $.get('https://pdm.pw/Upload', messages => {
+        messages.forEach(addMessage);
+    });
+}
+
+function postMessage(message) {
+    $.post('https://pdm.pw/Upload', message);
+}
