638786c4984e0e0f3e5c1c8e848091cabebed22a
mike yang <18604713262@163.com>
2021-12-24 16:33:08 -0500

    script commit

--- src/main.ts
+++ src/main.ts
@@ -179,4 +179,12 @@ function append_terminal_gr (a:String) {
   objDiv.scrollTop = objDiv.scrollHeight;
 }
 
-
+/**
+ * convert bytes to file size units
+ * */
+function bytesToSize(bytes) {
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   if (bytes == 0) return '0 Byte';
+   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
