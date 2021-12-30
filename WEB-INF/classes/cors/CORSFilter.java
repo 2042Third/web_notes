e14a84c9bcce994b49c3eddb823ea127fb905cad
Mike Yang <18604713262@163.com>
2021-12-30 13:11:36 -0500

    script commit from macos at Thu Dec 30 13:11:36 EST 2021

--- WEB-INF/classes/cors/CORSFilter.java
+++ WEB-INF/classes/cors/CORSFilter.java
@@ -19,7 +19,7 @@ public class CORSFilter extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
-        out.println(read_into_string("index.html"));
+        out.println(read_into_string("dist/pdm-notes/index.html"));
         response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
         response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     }
---
+++ gitp.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+now=$(date)
+git add -A
+git commit -m "script commit from macos at $now"
+git push origin main
