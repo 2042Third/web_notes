6af98acb1ada578fbbeed7c76e879121eed3fd72
Mike Yang <18604713262@163.com>
2022-01-04 09:45:33 -0500

    script commit from macos at Tue Jan  4 09:45:33 EST 2022

--- WEB-INF/classes/util/SendMail.java
+++ WEB-INF/classes/util/SendMail.java
@@ -1,19 +1,24 @@
 package util;
 import java.util.*;
-// import javax.mail.*;
 import javax.activation.DataSource;
-// import javax.mail.internet.*;
-// import com.sun.mail.util.PropUtil.*;
-// import javax.mail.internet.MimeMessage;
 
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.PasswordAuthentication;
-import jakarta.mail.Session;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Authenticator;
+
+// import jakarta.mail.Message;
+// import jakarta.mail.MessagingException;
+// import jakarta.mail.PasswordAuthentication;
+// import jakarta.mail.Session;
+// import jakarta.mail.Transport;
+// import jakarta.mail.internet.InternetAddress;
+// import jakarta.mail.internet.MimeMessage;
+// import jakarta.mail.Authenticator;
 
 public class SendMail {
     protected String hostname = "smtp-mail.outlook.com"; 
