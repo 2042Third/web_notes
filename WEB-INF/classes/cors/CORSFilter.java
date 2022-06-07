47d217ddc75cd1b6e4dd170a100e0d456db5079a
mike yang <18604713262@163.com>
2022-06-06 21:34:51 -0400

    script commit

--- WEB-INF/classes/cors/CORSFilter.java
+++ WEB-INF/classes/cors/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         // response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
-        out.println(read_into_string("/dist/pdm-notes/index.html"));
+        out.println(read_into_string("/dist/pdm-notes/en-US/index.html"));
         response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
         response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
 
